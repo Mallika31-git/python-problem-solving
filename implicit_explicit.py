b0a93dc8d8d6cd3974416adee9ed0ebff8cf840e
mallika <mallikaerpula@gmail.com>
2026-06-03 14:12:23 +0530

    added python explicit implicit practice programs

---
+++ implicit_explicit.py
@@ -0,0 +1,24 @@
+num1=input("enter a number1:")
+num2=input("enter a number2:")
+num1=int(num1)
+num2=int(num2)
+sum=num1+num2
+print(sum)
+#2
+x=9
+print(type(x))
+
+x=float(x)
+print(type(x))
+y=9.9
+print(type(y))
+y=int(y)
+print(type(y))
+z=9
+print(type(z))
+z=str(z)
+print(type(z))
+n="9"
+print(type(n))
+n=int(n)
+print(type(n))
